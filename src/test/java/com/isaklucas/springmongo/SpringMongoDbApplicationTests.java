73141f5c0c11d22abcbbeab35e1e776a786fba2c
isaklucas <isaklucas@gmail.com>
2022-08-24 10:17:18 -0300

    Project

---
+++ src/test/java/com/isaklucas/springmongo/SpringMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaklucas.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
