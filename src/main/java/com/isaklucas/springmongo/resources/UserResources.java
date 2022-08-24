a9ab618befba8e81f40fee579a5e0b4710218831
isaklucas <isaklucas@gmail.com>
2022-08-24 11:07:45 -0300

    DTOS

--- src/main/java/com/isaklucas/springmongo/resources/UserResources.java
+++ src/main/java/com/isaklucas/springmongo/resources/UserResources.java
@@ -1,6 +1,7 @@
 package com.isaklucas.springmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.isaklucas.springmongo.domain.User;
+import com.isaklucas.springmongo.dtos.UserDTO;
 import com.isaklucas.springmongo.services.UserService;
 
 @RestController
@@ -20,10 +22,11 @@ public class UserResources {
 	
 	
 	@GetMapping
-	public ResponseEntity<List<User>>  findAll() {
+	public ResponseEntity<List<UserDTO>>  findAll() {
 		
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 		
 	}
 
